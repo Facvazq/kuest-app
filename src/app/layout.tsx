1eef5c89fac74e2c3c5434098c66303dd5802143
Facundo Vazquez <facudev@Facundos-MacBook-Air.local>
2025-10-01 20:26:00 -0300

    Add Google AdSense account verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export const metadata: Metadata = {
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
     ]
+  },
+  other: {
+    'google-adsense-account': 'ca-pub-3536688103376580'
   }
 }
 
